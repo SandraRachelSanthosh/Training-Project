3c1fa1a6e393eb1f4b9194acb864887bf914dedf
Sandra Santhosh <sandra.santhosh@ibm.com>
2021-04-30 12:38:55 +0530

    added ProjectManagerAppUser

--- ProjectManagerAppUser/src/main/java/com/example/demo/controller/UserController.java
+++ ProjectManagerAppUser/src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.service.UserService;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,7 +12,15 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-    private List<User> allUsers = Arrays.asList(
+    @Autowired
+    UserService userService;
+    //creating a get mapping that retrieves all the users detail from the database
+    @GetMapping("/user")
+    private List<User> getAllUsers()
+    {
+        return userService.getAllUsers();
+    }
+    /*private List<User> allUsers = Arrays.asList(
             new User("1","Sandra"),
             new User("2","Anna")
     );
@@ -28,5 +36,31 @@ public class UserController {
                        .findFirst()
                        .orElseThrow(IllegalAccessException::new);
     }
+*/
+    @GetMapping ("/book/{bookid}")
+    private User getUsers(@PathVariable("userid") int userid)
+    {
+        return userService.getUsersById(userid);
+    }
 
+    @DeleteMapping("/user/{userid}")
+    private void deleteUser(@PathVariable("userid") int bookid)
+    {
+        userService.delete(bookid);
+    }
+
+
+    @PostMapping("/users")
+    private int saveUser(@RequestBody User users)
+    {
+        userService.saveOrUpdate(users);
+        return users.getUserId();
+    }
+
+    @PutMapping("/users")
+    private User update(@RequestBody User users)
+    {
+        userService.saveOrUpdate(users);
+        return users;
+    }
 }
--- ProjectManagerAppUser/src/main/java/com/example/demo/model/User.java
+++ ProjectManagerAppUser/src/main/java/com/example/demo/model/User.java
@@ -1,12 +1,28 @@
 package com.example.demo.model;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class User {
+    @Id
+    @Column
     private String userId;
+    @Column
     private String name;
 
     public User(String userId, String name) {
         this.userId = userId;
         this.name = name;
+
+    }
+
+    public User() {
 
     }
 
